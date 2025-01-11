985ec7cd8dec9027140f356bfe4c3175b05a95b7
mpenaEVG <mauriciopenadominguez.guadalupe@alumnado.fundacionloyola.net>
2025-01-11 18:30:19 +0100

    añadiendo diferentes parametros que debe cumplir la contraseña

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-import string 
- 
+import string
+
 password = "helloworld"
 
-upper_case = [1 if c in string.ascii_uppercase else 0 for c in password]
+upper_case = any([1 if c in string.ascii_uppercase else 0 for c in password]) 
+lower_case = any([1 if c in string.ascii_lowercase else 0 for c in password]) 
+special = any([1 if c in string.punctuation else 0 for c in password]) 
+upper_case = any([1 if c in string.digits else 0 for c in password]) 
+
+
+print(string.digits)
+
