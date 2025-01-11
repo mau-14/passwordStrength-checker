708d7ae43b0b94abb6cce8a21ad437dcdab31946
mpenaEVG <mauriciopenadominguez.guadalupe@alumnado.fundacionloyola.net>
2025-01-11 18:55:58 +0100

    verificamos y sumamos puntos

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import string
 
-password = "helloworld"
+password = "helloWorld"
 
 upper_case = any([1 if c in string.ascii_uppercase else 0 for c in password]) 
 lower_case = any([1 if c in string.ascii_lowercase else 0 for c in password]) 
@@ -26,3 +26,25 @@ if length > 12:
 
 if length > 17:
     score += 1
+
+print(f"La longitud de la contraseña es {str(length)}, añadimos {str(score)} puntos!")
+
+if sum(characters) > 1:
+    score += 1
+
+if sum(characters) > 2:
+    score += 1
+
+if sum(characters) > 3:
+    score += 3
+
+print(f"La contraseña tienen {str(sum(characters))} tipos diferente de caracter, añadimos {str(sum(characters))}")
+
+if score < 5:
+    print(f"La contraseña es muy débil. Score: {str(score)}/10")
+elif score == 5:
+    print(f"La contraseña esta ok! Score: {str(score)}/10")
+elif score > 5 and score < 8:
+    print(f"La contraseña esta muy bien! Score: {str(score)}/10")
+elif score == 10:
+    print(f"La contraseña es muy fuerte! Score: {str(score)}/10")
